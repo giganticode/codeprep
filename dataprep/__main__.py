05c6ce72f984efb819df26ff53f7aae687724a86
hlib <hlibbabii@gmail.com>
2019-04-10 09:16:25 +0300

    minor changes in cli

--- dataprep/__main__.py
+++ dataprep/__main__.py
@@ -1,13 +1,13 @@
 """Dataprep.
 
 Usage:
-  dataprep.py (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] nosplit
-  dataprep.py (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] chars
-  dataprep.py (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] basic
-  dataprep.py (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] basic+numbers
-  dataprep.py (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] bpe (1k | 5k | 10k)
-  dataprep.py (-h | --help)
-  dataprep.py --version
+  dataprep (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] nosplit
+  dataprep (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] chars
+  dataprep (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] basic
+  dataprep (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] basic+numbers
+  dataprep (-p <path> [-o <path-out>] | <text>) [--no-str] [--no-com] [--no-spaces] [--no-unicode] [--no-case] bpe (1k | 5k | 10k)
+  dataprep (-h | --help)
+  dataprep --version
 
 Options:
    -h, --help                                   Show this screen.
@@ -31,17 +31,17 @@ from docopt import docopt
 
 from dataprep import parse_projects, api, to_repr
 from dataprep.api import create_prep_config_from_args
-from dataprep.config import version
-
+from dataprep.config import version, app_name
 
 logger = logging.getLogger(__name__)
+
 
 def parse_run_options(arguments):
     return arguments
 
 
 def parse_command_line(argv):
-    arguments = docopt(__doc__, argv, version=f'Dataprep {version}')
+    arguments = docopt(__doc__, argv, version=f'{app_name} {version}')
     return create_prep_config_from_args(arguments), parse_run_options(arguments)
 
 
@@ -64,5 +64,9 @@ def run(argv):
         print(f"The output is ready at {os.path.abspath(output_path)}")
 
 
-if __name__ == '__main__':
+def main():
     run(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
