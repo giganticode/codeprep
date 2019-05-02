9f4fe47e0026ca2d8b5c73d1bbd7f5b3990895f4
hlib <hlibbabii@gmail.com>
2019-05-02 06:59:38 +0300

    simplify and make parsing-preprocessing steps more structured; support parsing of only 1 file; create abstraction of Dataset (and SubDataset)

--- dataprep/config.py
+++ dataprep/config.py
@@ -25,8 +25,11 @@ with open(os.path.join(root_package_dir, 'VERSION')) as version_file:
 USER_CONFIG_DIR = appdirs.user_config_dir(app_name, appauthor=False, version=version)
 USER_CACHE_DIR = appdirs.user_cache_dir(app_name, appauthor=False, version=version)
 
-DEFAULT_PARSED_DATASETS_DIR = os.path.join(USER_CACHE_DIR, 'prep_datasets')
+DEFAULT_PARSED_DATASETS_DIR = os.path.join(USER_CACHE_DIR, 'parsed_datasets')
+DEFAULT_PREP_DATASETS_DIR = os.path.join(USER_CACHE_DIR, 'prep_datasets')
 DEFAULT_BPE_DIR = os.path.join(data_dir, BPE_DIR)
+USER_BPE_DIR = os.path.join(USER_CONFIG_DIR, BPE_DIR)
 DEFAULT_BPE_CACHE_DIR = os.path.join(USER_CACHE_DIR, BPE_DIR)
 
 REWRITE_PARSED_FILE=False
+REWRITE_PREPROCESSED_FILE=False
---
+++ dataprep/split/bpe_config.py
@@ -0,0 +1,2 @@
+class BpeConfig:
+    pass # this is a stub for version 0.1.x. BpeConfig will be implemented in 1.0.x
