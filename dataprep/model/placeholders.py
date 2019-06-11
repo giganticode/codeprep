d431bd130faf2e139e05191da43fbe92c012686e
hlib <hlibbabii@gmail.com>
2019-06-11 18:42:38 +0300

    temporarily roll back to old placeholders for compatibily with earlier trained models

--- dataprep/model/placeholders.py
+++ dataprep/model/placeholders.py
@@ -1,14 +1,14 @@
 placeholders = {
     'comment': '<comment>',
-    'string_literal': '<str-literal>',
-    'word_start': '<w>',
-    'word_end': '</w>',
-    'capital': '<Cap>',
-    'capitals': '<CAPS>',
-    'ect': '<EOF>',
-    'non_eng': '<non-en>',
-    'non_eng_content': '<non-en-content>',
-    'olc_end': '<EOL>',
+    'string_literal': '<str_literal>',
+    'word_start': '`w',
+    'word_end': 'w`',
+    'capital': '`C',
+    'capitals': '`Cs',
+    'ect': '``',
+    'non_eng': '`E',
+    'non_eng_content': '`Es',
+    'olc_end': 'm`',
 }
 
 placeholders_beautiful = {
