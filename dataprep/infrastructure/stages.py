0a49b16ed2e7ebf3e3c4295fd9326b65a5c4393a
Hlib <hlibbabii@gmail.com>
2019-08-12 12:21:28 +0200

    do not check if vocab file is up-to-date, just dont recompute vocab if it already exists

--- dataprep/infrastructure/stages.py
+++ dataprep/infrastructure/stages.py
@@ -1,5 +1,6 @@
 """This module runs different stages of preprocessing flow and makes sure not to rerun a stage if its results are already available.
 """
+import os
 import logging
 from typing import Optional
 
@@ -48,6 +49,10 @@ def run_until_base_bpe_vocab(dataset: Dataset, custom_bpe_config: Optional[Custo
 
 
 def run_until_vocab(dataset: Dataset, custom_bpe_config: Optional[CustomBpeConfig]=None) -> None:
+    if os.path.exists(dataset.path_to_vocab_file):
+        logger.info("Vocabulary is already computed and up-to-date")
+        return
+
     if not is_path_ready(dataset.path_to_vocab_file):
         run_until_preprocessing(dataset, custom_bpe_config)
         logger.info("Computing vocab...")
@@ -58,4 +63,4 @@ def run_until_vocab(dataset: Dataset, custom_bpe_config: Optional[CustomBpeConfi
         archive_path(dataset.path_to_bpe_vocab_file)
         calc_vocab(dataset.preprocessed.path, dataset.preprocessed.file_iterator(), dataset.vocab_path)
     else:
-        logger.info("Vocabulary is already computed and up-to-date")
+        raise AssertionError()
