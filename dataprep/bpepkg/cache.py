91b6224297dc02273e5194e8c1c5cfff9eeaa241
hlib <hlibbabii@gmail.com>
2019-11-18 22:49:16 +0100

    add minor refactoring and doctest to cache.py

--- dataprep/bpepkg/cache.py
+++ dataprep/bpepkg/cache.py
@@ -1,10 +1,20 @@
+"""
+>>> import tempfile
+>>> f = tempfile.NamedTemporaryFile(delete=False)
+>>> cache = {'ab': ['a', 'b'], '\\t\\xa0': ['\\t', '\\xa0']}
+>>> dump_bpe_cache(cache, f.name)
+>>> cache == read_bpe_cache(f.name)
+True
+"""
+from typing import List, Dict
+
 from dataprep.util import to_literal_str, to_non_literal_str
 
 KEY_VALUE_DELIM = '\t'
 VALUE_PARTS_DELIM = ' '
 
 
-def read_bpe_cache(file: str):
+def read_bpe_cache(file: str) -> Dict[str, List[str]]:
     words = {}
     with open(file, 'r') as f:
         for line in f:
@@ -15,7 +25,8 @@ def read_bpe_cache(file: str):
     return words
 
 
-def dump_bpe_cache(dct, file):
+def dump_bpe_cache(dct: Dict[str, List[str]], file: str) -> None:
     with open(file, 'w') as f:
         for word, subwords in dct.items():
-            f.write(f'{to_literal_str(str(word))}{KEY_VALUE_DELIM}{to_literal_str(" ".join(subwords))}\n')
+            a = to_literal_str(" ".join(subwords))
+            f.write(f'{to_literal_str(str(word))}{KEY_VALUE_DELIM}{a}\n')
