37f5d84b272697da15ad007512193ff1d52a6a07
hlib <hlibbabii@gmail.com>
2020-05-27 18:58:26 +0200

    pre-processing fix

--- codeprep/pipeline/to_repr.py
+++ codeprep/pipeline/to_repr.py
@@ -64,7 +64,7 @@ def preprocess_and_write(params: Tuple[bytes, bytes, PrepConfig, str]):
     with gzip.GzipFile(src_file_path, 'rb') as i, open(not_finished_dest_file_path, 'w') as o:
         token_list = pickle.load(i)
         preprocessing_result = to_repr(prep_config, token_list + [SpecialToken(placeholders['ect'])], get_global_bpe_data_if_available())
-        o.write(to_literal_str(to_token_str(preprocessing_result.tokens)) + '\n')
+        o.write(to_literal_str(to_token_str(preprocessing_result.prepped_tokens.tokens)) + '\n')
 
     if part_nonbpe_vocab_folder:
         save_non_processable_tokens(preprocessing_result.non_processable_tokens, os.path.join(part_nonbpe_vocab_folder, f'{os.path.basename(dest_file_path)}_-_{time.time()}'))
